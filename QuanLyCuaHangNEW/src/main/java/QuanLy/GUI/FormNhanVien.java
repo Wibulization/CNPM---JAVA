9bc463c336fb334f846d6548869030692c590f02
Wibulization <nhmkhoi123@gmail.com>
2023-03-23 19:13:03 +0700

    ad

--- QuanLyCuaHangNEW/src/main/java/QuanLy/GUI/FormNhanVien.java
+++ QuanLyCuaHangNEW/src/main/java/QuanLy/GUI/FormNhanVien.java
@@ -121,9 +121,9 @@ public class FormNhanVien extends javax.swing.JPanel {
         jLabel3.setForeground(new java.awt.Color(0, 0, 0));
         jLabel3.setText("Mã nhân viên:");
 
-        txtMaNV.setEditable(false);
+        txtMaNV.setEditable(true);
         txtMaNV.setFont(new java.awt.Font("Roboto", 0, 15)); // NOI18N
-        txtMaNV.setEnabled(false);
+        txtMaNV.setEnabled(true);
 
         jLabel6.setFont(new java.awt.Font("Roboto", 1, 16)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(0, 0, 0));
